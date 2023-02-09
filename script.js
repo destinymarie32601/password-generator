8ad73920edc874a32eb71bb1765683db93a84d23
destiny32601 <destiny32601@gmail.com>
2023-02-08 19:08:20 -0800

    added javascript variables for possible password options

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength = window.prompt("How long would you like your password");
+var lowerCase = window.confirm("Would you like your password to contain lowercase letters?");
+var upperCase = window.confirm("Would you like your password to include uppercase letters?");
+var numbers = window.confirm("Would you like your password to contain numbers?");
+var specialChar = window.confirm("Would you like your password to include special characters?");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
